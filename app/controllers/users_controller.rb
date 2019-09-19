9f21cdfd7ccde9dd3af46205af1201ebf9281196
Trevahr Robertson <Trevahr@gmail.com>
2019-09-19 14:38:42 -0400

    user controller serialized

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,43 @@ class UsersController < ApplicationController
 
     def show
         user = User.find(params[:id])
-        render json: user
+        render json: user.to_json(user_serializer)
+    end
+
+
+    private
+
+    def user_serializer
+        {
+            :only => [:id, :name],
+            :include => {:tasks => 
+                {
+                    :only => [:name, :description, :specialty_id], 
+                        :include => {:specialty => 
+                        {
+                        :only => [:name, :id]
+                        },
+                        :bids => 
+                        {
+                        :only => [:name, :price, :status, :contractor_id],
+                            :include => {
+                                :contractor => {
+                                    :only => [:name, :id]
+                                }
+                            }
+
+                        } 
+                        
+
+                        }
+
+                }
+            }
+        }
     end
 
 
 end
+
+
+
