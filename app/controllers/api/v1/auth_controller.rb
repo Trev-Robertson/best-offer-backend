13e500d9d816e411073cd2148ee2f2dc60f0c483
Trevahr Robertson <Trevahr@gmail.com>
2019-09-21 17:56:36 -0400

    set up secret key and got encoding working

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::AuthController < ApplicationController
     
     def create
         @user = User.find_by({name: params[:name]})
-       
+        
        if @user && @user.authenticate(params[:password])
         
            render json: {authenticated: true, user: @user.user_serializer}, status: :accepted
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::API
+
+
+    def secret_key
+        Rails.application.credentials.dig(:secret_auth_key)
+    end
+
+    def encode(payload)
+        JWT.encode(payload, secret_key, 'HS512')
+    end
+    
+    def decode(token)
+        JWT.decode(token, secret_key, true, {algorithm: 'HS512'})[0]
+    end
+
+
 end
