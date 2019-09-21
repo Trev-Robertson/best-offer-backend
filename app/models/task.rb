798e3139627d66e31edf1123660e1985b2e82871
Trevahr Robertson <Trevahr@gmail.com>
2019-09-21 19:38:38 -0400

    added contractor name to profile

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,6 @@ class Task < ApplicationRecord
   belongs_to :user
   belongs_to :specialty
   has_many :bids, dependent: :destroy
+  validates :name, presence: true
+  validates :description, presence: true
 end
