b03a5a368a96668335d58d4b1d3d71fb07cffd77
Trevahr Robertson <Trevahr@gmail.com>
2019-09-23 18:59:44 -0400

    no login

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -4,9 +4,9 @@ class Api::V1::AuthController < ApplicationController
     
     def create
         @user = User.find_by({name: params[:name]})
-        
-       if @user && @user.authenticate(params[:password])
+        if @user && @user.authenticate(params[:password])
             token = encode({user_id: @user.id})
+            
            render json: {authenticated: true, 
             user: @user.user_serializer,
             token: token
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,7 +6,6 @@ class Api::V1::UsersController < ApplicationController
 
     def show
         token = request.headers["authentication"].split(" ")[1]
-        # byebug
         @user = User.find(decode(token)['user_id'])
         render json: @user.user_serializer, status: :accepted
     end
