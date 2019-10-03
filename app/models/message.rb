6b3438d925a7928c7ff34cc2b7c84aa2922acdad
Trevahr Robertson <Trevahr@gmail.com>
2019-10-03 08:54:48 -0400

    fixed some edge cases with improper input

--- app/models/message.rb
+++ app/models/message.rb
@@ -10,9 +10,10 @@ class Message < ApplicationRecord
           if arr.length > 1
           task_id = arr[0].tr('^A-Za-z0-9', '').to_i
           bid_amount = arr[1].tr('^A-Za-z0-9', '').to_i
-            if (Task.find(task_id) && bid_amount > 0)
-              
-              task = Task.find(task_id)
+        
+          if (!Task.find_by(id: task_id).blank? && bid_amount > 0)
+            bybug
+            task = Task.find(task_id)
               user = User.find(task.user_id)
            
               bid = task.bids.find {|b| b.contractor_id == contractor.id }
